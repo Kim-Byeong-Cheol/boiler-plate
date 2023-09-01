58fe6a9574f1ddabe8160f47a8d9c4ccc1d4755c
noma <noma@hancom.com>
2023-09-01 18:11:42 +0900

    처음으로 저장소로 올림

---
+++ Readme.md
@@ -0,0 +1,18 @@
+index.js 구성 참조 사이트:
+https://expressjs.com/ko/starter/hello-world.html
+
+port: 5000
+
+mongoDB-Atlas
+M0
+DB명: boilerPlate
+Username: haneolPa
+password: haneoly0516
+
+mongodb+srv://haneolPa:haneoly0516@boilerplate.owuyqiv.mongodb.net/?retryWrites=true&w=majority
+
+npm install mongoose --save-dev
+
+
+
+
---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => console.log('MongoDB connected!!'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
