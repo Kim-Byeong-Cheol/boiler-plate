834776abfd965405e237aa4489a50aed6a27f1f7
noma <noma@hancom.com>
2023-09-04 10:46:22 +0900

    password 저장을 hash 값으로 저장하도록 변경

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +32,28 @@ const userSchema = mongoose.Schema({
     },
     tokenExp: {
         type: Number
+    }
+});
+
+userSchema.pre("save", function (next) {
+    // save 함수가 실행되거전에 무엇을 한다는 의미
+    let user = this;
+
+    if (user.isModified("password")) {
+        bcrypt.genSalt(saltRounds)
+        .then(result => {
+            bcrypt.hash(user.password, result)
+            .then(hash => {
+                user.password = hash;
+                next();
+            })
+            .catch(err => {
+                return next(err);
+            });
+        })
+        .catch(err => {
+            return next(err);
+        });
     }
 });
 
